a63a933dea47f39df84b84e3fccf942c27a693e4
aditya-jha-material <141742732+aditya-jha-material@users.noreply.github.com>
2024-11-15 19:40:53 +0530

    finalised the project

--- client/src/theme/constant.tsx
+++ client/src/theme/constant.tsx
@@ -1,1 +1,1 @@
-export const apiURL = "http://172.31.47.209:5000"
+export const apiURL = "http://localhost:5000"
--- server/server.js
+++ server/server.js
@@ -313,5 +313,5 @@ app.post('/login', async (req, res) => {
 
 // Start Server
  const PORT = process.env.PORT || 5000;
- app.listen(PORT,"0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // module.exports = app;
